7a49437708a65e2c2d08a313a4c568a72425bdfd
phuyalbish <phuyalbish>
2023-02-25 15:10:02 +0545

    The third project

---
+++ p01_profile/android/app/src/main/kotlin/com/example/p01_profile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.p01_profile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ p03_calculator/android/app/src/main/kotlin/com/example/p03_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.p03_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
