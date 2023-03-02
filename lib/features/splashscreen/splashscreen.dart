1adea4b8ed5e1919fb1dc9becf2200b252dcbef1
BabishShrestha <babishshrestha8@gmail.com>
2023-03-02 16:14:54 +0545

    added manual splash screen

---
+++ lib/features/splashscreen/splashscreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../portfolio_home/presentation/portfolio_overview_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        const Duration(seconds: 5),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const FlutterFolioHome())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/unity.png'),
+          const SizedBox(height: 20),
+          const CircularProgressIndicator(
+            color: Colors.black,
+          )
+        ],
+      ),
+    ));
+  }
+}
